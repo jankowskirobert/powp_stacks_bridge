aff6af7a429e49177c8698316a0fd219fb8ad7e3
Robert Jankowski <jankowski.r@hotmail.com>
2017-03-15 21:40:41 +0100

    Ograniczenie widocznosci pol w klasie Node, wygenerowanie settera dla klasy Stack --Realizacja podpunktu 7 i 6

--- src/main/java/edu/kis/vh/stacks/Stack.java
+++ src/main/java/edu/kis/vh/stacks/Stack.java
@@ -35,4 +35,8 @@ public class Stack {
         return items[total--];
     }
 
+    public int getTotal() {
+        return total;
+    }
+
 }
--- src/main/java/edu/kis/vh/stacks/list/Node.java
+++ src/main/java/edu/kis/vh/stacks/list/Node.java
@@ -2,8 +2,8 @@ package edu.kis.vh.stacks.list;
 
 public class Node {
 
-    protected int value;
-    protected Node previous, next;
+    int value;
+    Node previous, next;
 
     public Node(int i) {
         value = i;
