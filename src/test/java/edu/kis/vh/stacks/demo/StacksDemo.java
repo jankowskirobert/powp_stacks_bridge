3ca5093f19d5dd4cd6000144b47fab1a293c4815
Robert Jankowski <jankowski.r@hotmail.com>
2017-03-15 22:22:42 +0100

    Wydzielenie metody testStacks

--- src/test/java/edu/kis/vh/stacks/demo/StacksDemo.java
+++ src/test/java/edu/kis/vh/stacks/demo/StacksDemo.java
@@ -6,52 +6,48 @@ import edu.kis.vh.stacks.factory.DefaultStacksFactory;
 
 class StacksDemo {
 
-    /**
-     * zbyt dużo tabulacji: były dwie powinna być jedna dla każdego wcięcia
-     * które wystąpiło. Kolejno dodanie brakujących klamer dla bloków
-     * decyzyjnych oraz pętli.
-     */
-    public static void main(String[] args) {
+	/**
+	 * zbyt dużo tabulacji: były dwie powinna być jedna dla każdego wcięcia
+	 * które wystąpiło. Kolejno dodanie brakujących klamer dla bloków
+	 * decyzyjnych oraz pętli.
+	 */
+	public static void main(String[] args) {
 
-        DefaultStacksFactory factory = new DefaultStacksFactory();
-        //uporządkowanie wszystkich elementów tablicy w jednej kolumnie
-        Stack[] stacks = {
-            factory.getStandardStack(),
-            factory.getFalseStack(),
-            factory.getFIFOStack(),
-            factory.getHanoiStack()
-        };
+		DefaultStacksFactory factory = new DefaultStacksFactory();
+		testStacks(factory);
+	}
 
-        for (int i = 1; i < 15; i++) {
-            for (int j = 0; j < 3; j++) {
-                stacks[j].push(i);
-            }
-        }
+	private static void testStacks(DefaultStacksFactory factory) {
+		Stack[] stacks = { factory.getStandardStack(), factory.getFalseStack(), factory.getFIFOStack(),
+				factory.getHanoiStack() };
 
-        java.util.Random random = new java.util.Random();
-        for (int i = 1; i < 15; i++) {
-            stacks[3].push(random.nextInt(20));
-        }
+		for (int i = 1; i < 15; i++) {
+			for (int j = 0; j < 3; j++) {
+				stacks[j].push(i);
+			}
+		}
 
-        for (int i = 0; i < stacks.length; i++) {
-            while (!stacks[i].isEmpty()) {
-                System.out.print(stacks[i].pop() + "  ");
-            }
-            /**
-             * usunięcie tabulatora który mylnie wskazyważ, że ta linia jest
-             * częścią pętli while
-             */
-            System.out.println();
-        }
+		java.util.Random random = new java.util.Random();
+		for (int i = 1; i < 15; i++) {
+			stacks[3].push(random.nextInt(20));
+		}
 
-        /**
-         * Kombinacja klawiszy shift+alt+'strzałki kierunkowe' powodują dodanie
-         * wcięcia lub jego usunięcie
-         */
-        System.out.println("total rejected is "
-                + ((HanoiStack) stacks[3]).reportRejected()
-        );
+		for (int i = 0; i < stacks.length; i++) {
+			while (!stacks[i].isEmpty()) {
+				System.out.print(stacks[i].pop() + "  ");
+			}
+			/**
+			 * usunięcie tabulatora który mylnie wskazyważ, że ta linia jest
+			 * częścią pętli while
+			 */
+			System.out.println();
+		}
 
-    }
+		/**
+		 * Kombinacja klawiszy shift+alt+'strzałki kierunkowe' powodują dodanie
+		 * wcięcia lub jego usunięcie
+		 */
+		System.out.println("total rejected is " + ((HanoiStack) stacks[3]).reportRejected());
+	}
 
 }
