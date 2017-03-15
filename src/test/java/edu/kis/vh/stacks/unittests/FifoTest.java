5d593cc51168588cd8d0c194888eff5683ce04f0
Robert Jankowski <jankowski.r@hotmail.com>
2017-03-15 22:57:15 +0100

    Dodanie dodatkowych testow dla klas HanoiStack i FifoStack

---
+++ src/test/java/edu/kis/vh/stacks/unittests/FifoTest.java
@@ -0,0 +1,27 @@
+package edu.kis.vh.stacks.unittests;
+
+import org.junit.Test;
+
+import edu.kis.vh.stacks.FifoStack;
+import junit.framework.Assert;
+
+public class FifoTest {
+
+	@Test
+	public void testPop() {
+		FifoStack stack = new FifoStack();
+		Assert.assertEquals(-1, stack.pop());
+	}
+	@Test
+	public void testPushAndPop() {
+		FifoStack stack = new FifoStack();
+		stack.push(5);
+		Assert.assertEquals(5, stack.pop());
+		stack.push(5);
+		stack.push(10);
+		stack.push(15);
+		Assert.assertEquals(5, stack.pop());
+		Assert.assertEquals(15, stack.top());
+		Assert.assertEquals(1, stack.getTotal());
+	}
+}
---
+++ src/test/java/edu/kis/vh/stacks/unittests/HanoiTest.java
@@ -0,0 +1,35 @@
+package edu.kis.vh.stacks.unittests;
+
+import org.junit.Test;
+
+import edu.kis.vh.stacks.HanoiStack;
+import junit.framework.Assert;
+
+public class HanoiTest {
+
+	@Test
+	public void testRejectDefault() {
+		HanoiStack stack = new HanoiStack();
+		final int REJECTED = 0;
+		Assert.assertEquals(REJECTED, stack.reportRejected());
+	}
+	@Test
+	public void testPushRejected() {
+		HanoiStack stack = new HanoiStack();
+		final int REJECTED = 1;
+		final int STACK_BEGIN = -1;
+		stack.push(STACK_BEGIN+1);
+		stack.push(Integer.MAX_VALUE);
+		Assert.assertEquals(REJECTED, stack.reportRejected());
+	}
+	@Test
+	public void testPush() {
+		HanoiStack stack = new HanoiStack();
+		final int REJECTED = 0;
+		stack.push(Integer.MAX_VALUE);
+		Assert.assertEquals(REJECTED, stack.reportRejected());
+		Assert.assertEquals(Integer.MAX_VALUE, stack.top());
+		Assert.assertEquals(Integer.MAX_VALUE, stack.pop());
+	}
+	
+}
